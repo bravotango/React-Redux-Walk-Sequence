d0f719e831ec7733b181cba4ca640cae5c318cda
seattleriot <seattleriot@hotmail.com>
2020-07-04 00:31:21 -0700

    removed missing navbar icon

--- src/App.js
+++ src/App.js
@@ -13,13 +13,6 @@ function App() {
         <div className="col-sm">
           <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
-              <img
-                src="/docs/4.4/assets/brand/bootstrap-solid.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt=""
-              />
               React | Redux | BootstrapCSS
             </a>
           </nav>
